6d56fd9100f1f34927a289109d241bc4d85001c0
Thierry Geindre <thierry.geindre@bedrockstreaming.com>
2026-02-14 11:38:57 +0100

    Fix billie

--- ui/tracks.go
+++ ui/tracks.go
@@ -7,14 +7,14 @@ import (
 func getBillieJeanBassTrack() *seq.Track {
 	t := seq.NewTrack(0, "Bass (Billie Jean - played version)")
 	t.Append(
-		seq.Step{AtTick: 0, Note: 64 - 24, Velocity: 110, GateTick: 24},   // F#
-		seq.Step{AtTick: 48, Note: 59 - 24, Velocity: 110, GateTick: 24},  // C#
-		seq.Step{AtTick: 96, Note: 62 - 24, Velocity: 110, GateTick: 24},  // E
-		seq.Step{AtTick: 144, Note: 64 - 24, Velocity: 110, GateTick: 24}, // F#
-		seq.Step{AtTick: 192, Note: 62 - 24, Velocity: 110, GateTick: 24}, // E
-		seq.Step{AtTick: 240, Note: 59 - 24, Velocity: 110, GateTick: 24}, // C#
-		seq.Step{AtTick: 288, Note: 57 - 24, Velocity: 110, GateTick: 24}, // B
-		seq.Step{AtTick: 336, Note: 59 - 24, Velocity: 110, GateTick: 24}, // C#
+		seq.Step{AtTick: 0, Note: 64 - 12, Velocity: 110, GateTick: 24},   // F#
+		seq.Step{AtTick: 48, Note: 59 - 12, Velocity: 110, GateTick: 24},  // C#
+		seq.Step{AtTick: 96, Note: 62 - 12, Velocity: 110, GateTick: 24},  // E
+		seq.Step{AtTick: 144, Note: 64 - 12, Velocity: 110, GateTick: 24}, // F#
+		seq.Step{AtTick: 192, Note: 62 - 12, Velocity: 110, GateTick: 24}, // E
+		seq.Step{AtTick: 240, Note: 59 - 12, Velocity: 110, GateTick: 24}, // C#
+		seq.Step{AtTick: 288, Note: 57 - 12, Velocity: 110, GateTick: 24}, // B
+		seq.Step{AtTick: 336, Note: 59 - 12, Velocity: 110, GateTick: 24}, // C#
 	)
 	t.SetLoop(true, 384)
 	t.SetChannel(0)
